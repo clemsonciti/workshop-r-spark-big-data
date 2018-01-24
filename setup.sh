f4b28030dddeff1e425059f31b284453f28caccc
linhbngo <lngo@clemson.edu>
2018-01-24 10:42:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,16 @@ else
   echo 'module load hdp' >> ~/.bashrc
   echo 'cypress-kinit' >> ~/.bashrc
 fi
+
+if grep -Fxq 'module load hdp' ~/.jhubrc
+then 
+  if grep -Fxq 'cypress-kinit' ~/.jhubrc
+  then
+    echo 'User account is properly configured'
+  else
+    echo 'cypress-kinit' >> ~/.jhubrc
+  fi
+else
+  echo 'module load hdp' >> ~/.jhubrc
+  echo 'cypress-kinit' >> ~/.jhubrc
+fi
